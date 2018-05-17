e1b6ed92361f02355d2a03261bdafc1226ad6d1b
Marcin Lewczyk <lewczyk.marcin@gmail.com>
2018-05-17 22:27:47 +0200

    ItemInbound activity work in progress

--- app/src/main/java/com/example/marcinlewczyk/mobilesystem/ItemInbound.java
+++ app/src/main/java/com/example/marcinlewczyk/mobilesystem/ItemInbound.java
@@ -11,14 +11,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.marcinlewczyk.mobilesystem.Config.WebServiceConfig;
 import com.example.marcinlewczyk.mobilesystem.POJO.ItemInboundInfo;
@@ -237,13 +235,12 @@ public class ItemInbound extends AppCompatActivity {
                             itemInboundInfo.getId() + "  Location: " + itemInboundInfo.getLocationName(),
                             itemQty + "");
             } catch (Exception e) {
-                Log.d("ItemInbound", e.toString());
                 new AlertDialog.Builder(ItemInbound.this)
                         .setTitle("Error")
-                        .setMessage("Check internet connection")
+                        .setMessage("Check Internet connection")
                         .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                new ItemInbound.ItemInfoWebServiceHandler().execute(WebServiceConfig.address);
+                                callItemInfoWebService();
                             }
                         }).setCancelable(false).setNegativeButton("Back", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -254,25 +251,111 @@ public class ItemInbound extends AppCompatActivity {
         }
     }
 
-    private void connectCommitButtonLogic(){
-        commitInboundButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startWebService();
-            }
-        });
-    }
-
-    private void startWebService(){
-
-    }
-
     private void updateGUI(String itemInfo, String fromInfo, String qtyInfo) {
         itemIdTxt.setText("Item: " + itemInfo);
         fromTxt.setText("From: " + fromInfo);
         itemQtyTxt.setText("Qty: " + qtyInfo);
     }
 
+    private void connectCommitButtonLogic(){
+        commitInboundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callItemInboundWebService();
+            }
+        });
+    }
+
+    private void callItemInboundWebService(){
+        new ItemInboundWebServiceHandler().execute(WebServiceConfig.address + "/additem/" + itemId +"/" + itemQty + "/" + currentLocation);
+    }
+
+    private class ItemInboundWebServiceHandler extends AsyncTask<String, Void, String>{
+
+        private ProgressDialog dialog = new ProgressDialog(ItemInbound.this);
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Connecting with server...");
+            dialog.show();
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setCancelable(false);
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                URLConnection connection = url.openConnection();
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(10000);
+
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+
+                return convertInputStreamToString(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+
+            dialog.dismiss();
+            try {
+                if(result == "true"){
+                    new AlertDialog.Builder(ItemInbound.this)
+                            .setTitle("Processed")
+                            .setMessage("Package successfully processed")
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    clearScreen();
+                                }
+                            }).setCancelable(false).setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).show();
+                } else {
+                    new AlertDialog.Builder(ItemInbound.this)
+                            .setTitle("Error")
+                            .setMessage("Server didn't processed this request, check data and try again!")
+                            .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    callItemInboundWebService();
+                                }
+                            }).setCancelable(false).setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).show();
+                }
+            } catch (Exception e) {
+                new AlertDialog.Builder(ItemInbound.this)
+                        .setTitle("Error")
+                        .setMessage("Check Internet connection")
+                        .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                callItemInboundWebService();
+                            }
+                        }).setCancelable(false).setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).show();
+            }
+        }
+    }
+
+    private void clearScreen(){
+        itemIdTxt.setText("");
+        fromTxt.setText("");
+        itemQtyTxt.setText("");
+        itemId = -1;
+        locationId = -1;
+        itemQty = -1;
+        commitInboundButton.setEnabled(false);
+    }
 
     public static String convertInputStreamToString(InputStream is) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
---
+++ app/src/main/java/com/example/marcinlewczyk/mobilesystem/POJO/ItemInboundInfo.java
@@ -0,0 +1,59 @@
+package com.example.marcinlewczyk.mobilesystem.POJO;
+
+public class ItemInboundInfo {
+
+    private long id;
+    private String name;
+    private String description;
+    private int quantity;
+    private long locationId;
+    private String locationName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(long locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
